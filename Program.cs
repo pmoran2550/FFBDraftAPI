2a1d50e1a24a6f16ec15f0c3e2c9da1c7e6b4416
pmoran2550 <patmoran@wi.rr.com>
2025-04-30 13:18:56 -0500

    Add multiple CORS origins.

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://yellow-ocean-036437710.6.azurestaticapps.net/")
             .AllowAnyHeader()
             .AllowCredentials()
             .AllowAnyMethod();
